fe582e47a773d6b6140789e713549fcce5ac2e69
prashxant <notxdead0@gmail.com>
2025-08-17 13:24:54 +0530

    init chanh dai for makeover

---
+++ src/config/site.ts
@@ -0,0 +1,39 @@
+import { USER } from "@/data/user";
+import type { NavItem } from "@/types/nav";
+
+export const SITE_INFO = {
+  name: USER.displayName,
+  url: process.env.APP_URL || "https://chanhdai.com",
+  ogImage: USER.ogImage,
+  description: USER.bio,
+  keywords: USER.keywords,
+};
+
+export const META_THEME_COLORS = {
+  light: "#ffffff",
+  dark: "#09090b",
+};
+
+export const MAIN_NAV: NavItem[] = [
+  {
+    title: "Daifolio",
+    href: "/",
+  },
+  {
+    title: "Blog",
+    href: "/blog",
+  },
+  {
+    title: "Components",
+    href: "/components",
+  },
+];
+
+export const SOURCE_CODE_GITHUB_REPO = "ncdai/chanhdai.com";
+export const SOURCE_CODE_GITHUB_URL = "https://github.com/ncdai/chanhdai.com";
+
+export const UTM_PARAMS = {
+  utm_source: "chanhdai.com",
+  utm_medium: "portfolio_website",
+  utm_campaign: "referral",
+};
