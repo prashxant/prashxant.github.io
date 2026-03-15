991593b9f3a7bfd4f88c13ab0e8315fb5f735413
prashxant <notxdead0@gmail.com>
2026-03-15 16:08:18 +0530

    animation

---
+++ app/page.tsx
@@ -0,0 +1,47 @@
+import { BlogPosts } from "components/posts";
+import { SpinningText } from "components/motion-primitives/spinning-text";
+import Image from "next/image";
+import { TextScramble } from "components/motion-primitives/text-scramble";
+
+export default function Page() {
+  return (
+    <section>
+      <div className="relative flex items-center justify-center w-[200px] h-[200px]">
+        <Image
+          className="aspect-square border mr-16 border-amber-100 rounded-full bg-white object-cover"
+          width={110}
+          height={110}
+          src="/pfpp.png"
+          alt="Profile picture"
+        />
+
+        <SpinningText radius={6.8} className="mr-16 absolute">
+          {`pre-order • pre-order • pre-order • `}
+        </SpinningText>
+      </div>
+
+      <TextScramble className="font-mono text-2xl font-semibold tracking-tighter ">
+        Prashant Sharma
+      </TextScramble>
+
+      <TextScramble
+        duration={1.5}
+        characterSet=". "
+        className="mb-8 text-md text-white/50 font-extralight"
+      >
+        Trying to understand tech
+      </TextScramble>
+
+      <p className="mb-4">
+        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
+        Vim's keystroke commands and tabs' flexibility for personal viewing
+        preferences. This extends to my support for static typing, where its
+        early error detection ensures cleaner code, and my preference for dark
+        mode, which eases long coding sessions by reducing eye strain.`}
+      </p>
+      <div className="my-8">
+        <BlogPosts />
+      </div>
+    </section>
+  );
+}
